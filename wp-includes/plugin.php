f377b0ef96d1ee51e3daf6838eceb50d2ae29ef5
Hans Fredrik Nordhaug <hansfn@gmail.com>
2014-11-20 22:26:23 +0100

    Upgraded Disqus plugin to 2.83.

--- wp-includes/plugin.php
+++ wp-includes/plugin.php
@@ -654,10 +654,12 @@ function remove_all_actions($tag, $priority = false) {
 function plugin_basename( $file ) {
 	global $wp_plugin_paths;
 
+	if (is_array($wp_plugin_paths)) {
 	foreach ( $wp_plugin_paths as $dir => $realdir ) {
 		if ( strpos( $file, $realdir ) === 0 ) {
 			$file = $dir . substr( $file, strlen( $realdir ) );
 		}
+	}
 	}
 
 	$file = wp_normalize_path( $file );
